303403dae5dd51f8080221f999202cab160c2bfc
Elmarath <65414809+Elmarath@users.noreply.github.com>
2020-12-06 21:16:53 +0300

    vize

---
+++ DenemeDosyasi/dnemeler.java
@@ -0,0 +1,9 @@
+package DenemeDosyasi;
+
+import java.lang.ProcessBuilder.Redirect.Type;
+
+public class dnemeler {
+    public static void main(String[] args) {
+        System.out.println("yeyy");
+    }
+}
--- Denemeler/denemeler.java
+++
@@ -1,7 +0,0 @@
-package Denemeler;
-
-public class denemeler {
-    public static void main() {
-        System.out.println("selam");
-    }
-}
